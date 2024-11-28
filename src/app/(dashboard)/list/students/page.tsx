4e6c4c021c332bea788be69988394ccefdd52a77
“benchesters” <benchesters@hotmail.com>
2024-11-28 11:32:29 +0700

    Modals built, forms next

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -77,9 +78,10 @@ const renderRow = (item:Student) => (
         </button>
         </Link>
         {role == "admin" && (
-        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-          <Image src="/delete.png" alt="" width={16} height={16} />
-        </button>
+        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+        //   <Image src="/delete.png" alt="" width={16} height={16} />
+        // </button>
+        <FormModal table="student" type="delete" id={item.id} />
         )}
         
       </div>
@@ -103,9 +105,10 @@ const renderRow = (item:Student) => (
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
        {role === "admin" && (
-         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button> 
+        //  <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+        //       <Image src="/plus.png" alt="" width={14} height={14} />
+        //     </button> 
+        <FormModal table="student" type="create"/>
                 )}
           </div>
         </div>
